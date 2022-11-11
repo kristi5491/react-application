864c7aac93436150a59536bd5b5d4faf8dd1bf4d
kristi5491 <ksavchin05@gmail.com>
2022-11-11 12:34:24 +0200

    create

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Navigation from './components/navigation';
-import Content from './components/content';
+
 
 function App () {
   return (
     <div className="App">
-      <Navigation />
-      <Content />
     </div>
   );
 }
